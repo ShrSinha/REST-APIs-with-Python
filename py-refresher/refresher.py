ad4f8eec6d73daf07d6a3ab4fcb82b1f1d7bab80
Shruti Sinha <shruti.sinha@gmail.com>
2023-04-26 22:07:32 -0700

    modified:   py-refresher/refresher.py

--- py-refresher/refresher.py
+++ py-refresher/refresher.py
@@ -1,6 +1,21 @@
-from typing import Optional
+from typing import Callable
+from functools import wraps
 
-class Student:
-    def __init__(self, name: str, grades: Optional[list[int]] = None) -> None:
-        self.name = name
-        self.grades = grades or []
+def decorator_factory(access_level):
+    @wraps(func)
+    def decorator(func: Callable) -> Callable:     
+        def inner(*args, *kwargs):
+            return func(*args, **kwargs)
+        return inner
+    return decorator    
+
+
+
+@decorator_factory("admin")
+def get_admin_password(name: str) -> str:
+    return "1234"
+
+
+@decorator_factory("user")
+def get_user_password(name: str) -> str:
+    return "5678"
